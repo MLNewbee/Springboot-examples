3d2d73d52e6d2b454bd675f4ff336add3fea82e7
MLNewbee <574052658@qq.com>
2019-04-16 23:44:11 +0800

    Springboot mail initial files

---
+++ Mail/src/main/java/com/hust/mail/MailService.java
@@ -0,0 +1,76 @@
+package com.hust.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@Service
+public class MailService {
+
+    @Value("${spring.mail.username}")
+    private  String From;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleMail(String To,String subject,String content){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(To);
+        message.setSubject(subject);
+        message.setText(content);
+        message.setFrom(From);
+
+        mailSender.send(message);
+    }
+
+    public  void  sendHtmlMail(String To,String subject,String content) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message,true);
+        helper.setFrom(From);
+        helper.setTo(To);
+        helper.setSubject(subject);
+        helper.setText(content);
+        mailSender.send(message);
+
+    }
+
+    public void  sendAttachedmentsMail(String To,String subject,String content,String filePath) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message,true);
+        helper.setFrom(From);
+        helper.setTo(To);
+        helper.setSubject(subject);
+        helper.setText(content);
+
+        FileSystemResource   file = new FileSystemResource(new File(filePath));
+        String filename  = file.getFilename();
+        helper.addAttachment(filename,file);
+        mailSender.send(message);
+
+    }
+
+    public void  sendInLineResourceMail(String To,String subject,String content,String rscPath,String rscId) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message,true);
+        helper.setFrom(From);
+        helper.setTo(To);
+        helper.setSubject(subject);
+        helper.setText(content);
+
+        FileSystemResource  resource = new FileSystemResource(new File(rscPath));
+        helper.addInline(rscId,resource);
+        mailSender.send(message);
+
+    }
+}
---
+++ Mail/src/test/java/com/hust/mail/MailApplicationTests.java
@@ -0,0 +1,45 @@
+package com.hust.mail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailApplicationTests {
+
+    @Resource
+    MailService mailService;
+
+    @Test
+    public  void sendHtmlMainTest() throws MessagingException {
+        String content = "<html>\n"+
+                "<body>\n"+
+                "<h3> Hello World</h3>\n"+
+                "</body>\n"+
+                "</html>";
+        mailService.sendHtmlMail("574052658@qq.com","HTML 邮件",content);
+    }
+    @Test
+    public  void sendAttachedmentsMainTest() throws MessagingException {
+        String filePath = "C:/Users/menglong/Desktop/Test.txt";
+        mailService.sendAttachedmentsMail("574052658@qq.com","这是一封带附件的邮件",
+                "这是一封带附件的邮件的内容",filePath);
+    }
+
+    @Test
+    public void sendInLineResourceMailTest() throws MessagingException {
+        String imgPath = "C:\\Users\\menglong\\Desktop\\Fig6A.jpg";
+        String rscId = "01";
+        String content = "<html><body> 这是有照片的邮件：<img src=\'cid:"+rscId
+                +"\'> </img></body></html>";
+
+        mailService.sendInLineResourceMail("574052658@qq.com","带有图片的文件",
+                content,imgPath,rscId);
+    }
+
+}
