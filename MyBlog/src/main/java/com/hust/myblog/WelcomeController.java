5c7424143445d7a7dad5511cbf6acacf46823e1d
MLNewbee <574052658@qq.com>
2019-05-02 23:50:08 +0800

    MyBlog Project

--- MyBlog/src/main/java/com/hust/myblog/WelcomeController.java
+++ MyBlog/src/main/java/com/hust/myblog/WelcomeController.java
@@ -1,4 +1,19 @@
 package com.hust.myblog;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class WelcomeController {
+
+    @RequestMapping(value = "/i")
+    public String index(){
+        return "/index.html";
+    }
+
+    @RequestMapping(value = "/welcome")
+    public String home(){
+        return "views/welcome";
+    }
 }
